4fdc50e4a9523c1c15d60db6d46ad02d5ea83de4
Eugene <38591759+p1q@users.noreply.github.com>
2020-01-22 20:07:46 +0200

    Update ex16.sql

--- ex16.sql
+++ ex16.sql
@@ -1,4 +1,3 @@
-SELECT hd
-FROM PC
-GROUP BY hd
-HAVING COUNT(model) > 1
+SELECT DISTINCT PC1.model, PC2.model, PC1.speed, PC2.ram
+FROM PC PC1, PC PC2
+WHERE PC1.speed = PC2.speed AND PC1.ram = PC2.ram AND PC1.model > PC2.model
